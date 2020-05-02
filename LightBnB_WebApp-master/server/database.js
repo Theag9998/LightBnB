caf8ae06bde51384f96d10e6a801220c0abd6fb9
Thea Ganden <wtp9998@hotmail.com>
2020-05-02 20:04:26 +0000

    Obtain all users reservations from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,7 +65,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+return pool.query(`
+SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+FROM reservations
+JOIN properties ON properties.id = reservations.property_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1 AND reservations.end_date < NOW()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT $2;`, [guest_id, limit])
+.then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
