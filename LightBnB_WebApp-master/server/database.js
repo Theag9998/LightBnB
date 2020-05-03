35e3ac8f263aa8c1cc25dd169b2f37073d418acd
Thea Ganden <wtp9998@hotmail.com>
2020-05-03 18:48:00 +0000

    Fix bugs in database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -100,48 +100,47 @@ const getAllProperties = function(options, limit = 10) {
    // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += ` WHERE city LIKE $${queryParams.length} `;
   }
 
   if(options.owner_id) {
-    queryParams.push(`%${options.owner_id}%`);
-    if (queryParams.length === 1) {
-      queryString += `WHERE properties.owner_id = $${queryParams.length} `;
-    } else {
-      queryString += `AND properties.owner_id = $${queryParams.length} `;
-    }
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` WHERE properties.owner_id = $${queryParams.length} `;
+    
   }
 
   if(options.minimum_price_per_night) {
-    queryParams.push(`%${options.minimum_price_per_night}%`);
+    queryParams.push(`${options.minimum_price_per_night}`);
     if (queryParams.length === 1) {
-      queryString += `WHERE properties.cost_per_night > $${queryParams.length} `;
+      queryString += ` WHERE properties.cost_per_night > $${queryParams.length} `;
     } else {
-      queryString += `AND properties.cost_per_night > $${queryParams.length}`;
+      queryString += ` AND properties.cost_per_night > $${queryParams.length}`;
     }
   }
 
   if(options.maximum_price_per_night) {
-    queryParams.push(`%${options.maximum_price_per_night}%`);
+    queryParams.push(`${options.maximum_price_per_night}`);
     if (queryParams.length === 1) {
-      queryString += `WHERE properties.cost_per_night < $${queryParams.length} `;
+      queryString += ` WHERE properties.cost_per_night < $${queryParams.length} `;
     } else {
-      queryString += `AND properties.cost_per_night < $${queryParams.length}`;
+      queryString += ` AND properties.cost_per_night < $${queryParams.length}`;
     }
   }
 
-  queryString += `GROUP BY properties.id`;
+  queryString += ` GROUP BY properties.id`;
 
   
   if(options.minimum_rating) {
-    queryParams.push(`%${options.minimum_rating}%`);
-    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
+    queryParams.push(`${options.minimum_rating}`);
+    //if (queryParams.length === 1) {
+      queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
+    // } else {
+    //   queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
+    // }
   }
-
    // 4
    queryParams.push(limit);
    queryString += `
-   GROUP BY properties.id
    ORDER BY cost_per_night
    LIMIT $${queryParams.length};
    `;
@@ -162,9 +161,15 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+return pool.query(`
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+  cost_per_night, street, city, province, post_code, country, parking_spaces, 
+  number_of_bathrooms, number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
+    property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, 
+    property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(res => res.rows[0]);
+
+};
 exports.addProperty = addProperty;
